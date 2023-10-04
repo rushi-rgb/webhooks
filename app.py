3ee4dde5dbefb491dab0ebf2a43096f41c0d595c
rndudhe1808 <141106013+rndudhe1808@users.noreply.github.com>
2023-10-04 09:43:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import http.server
 import socketserver
 
 PORT = 8080
-MESSAGE = "Hello from my Docker container!"
+MESSAGE = "Hello V1"
 
 class MyHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
