1b2a3453dab4019a04506cdc608afc24f97b1488
rndudhe1808 <141106013+rndudhe1808@users.noreply.github.com>
2023-10-04 09:28:07 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+import http.server
+import socketserver
+
+PORT = 8080
+MESSAGE = "Hello from my Docker container!"
+
+class MyHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
+        self.wfile.write(MESSAGE.encode())
+
+with socketserver.TCPServer(("", PORT), MyHandler) as httpd:
+    print(f"Server listening on port {PORT}")
+    httpd.serve_forever()
