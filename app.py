9ca366f91a53bf3c300089a7f25582c3de981539
rndudhe1808 <141106013+rndudhe1808@users.noreply.github.com>
2023-10-04 10:44:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import http.server
 import socketserver
 
 PORT = 8080
-MESSAGE = "Hello from Github version V3"
+MESSAGE = "Hello from Github version V3.0"
 
 class MyHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
