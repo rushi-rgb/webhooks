8a982be4d5e43a135038cb9ba3c6b3a7f5dd73f9
Rushikesh Gore <84717481+rushi-rgb@users.noreply.github.com>
2023-10-10 09:12:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ import socketserver
 
 PORT = 8080
 MESSAGE = "Hello from S2i Application Version 1"
+MESSAGE = "Hello from S2i Application Version 2"
+
+
 
 class MyHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
